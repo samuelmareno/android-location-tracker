5a8f04196446983431fd48d2737cbb7851a1ffdb
Samuel Mareno <samuelmareno777@gmail.com>
2022-10-09 18:34:36 +0700

    Initial commit

---
+++ app/src/main/java/id/mareno/locationtracker/MainActivity.kt
@@ -0,0 +1,56 @@
+package id.mareno.locationtracker
+
+import android.Manifest
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import id.mareno.locationtracker.ui.theme.LocationTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            0
+        )
+        setContent {
+            LocationTrackerTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(onClick = {
+                        Intent(applicationContext, LocationService::class.java).apply {
+                            action = LocationService.ACTION_START
+                            startService(this)
+                        }
+                    }) {
+                        Text(text = "Start")
+                    }
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Button(onClick = {
+                        Intent(applicationContext, LocationService::class.java).apply {
+                            action = LocationService.ACTION_STOP
+                            startService(this)
+                        }
+                    }) {
+                        Text(text = "Stop")
+                    }
+                }
+            }
+        }
+    }
+}
